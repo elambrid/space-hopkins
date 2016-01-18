fc49e11d72ae56fd629de236bd22c4ade829d407
Erini <elambrid@gmail.com>
2016-01-17 19:28:12 -0500

    some landing page changes

--- js/main.js
+++ js/main.js
@@ -86,3 +86,33 @@ $(window).load(function() {
     var triggerheight = Math.round(boxheight/itemlength+1);
 	$('#myCarousel .list-group-item').outerHeight(triggerheight);
 });
+
+$(function () {
+
+    "use strict";
+
+    var $bgobj = $(".ha-bg-parallax"); // assigning the object
+
+    $(window).on("scroll", function () {
+
+        var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
+
+        // Put together our final background position
+
+        var coords = '100% ' + yPos + 'px';
+
+        // Move the background
+
+        $bgobj.css({ backgroundPosition: coords });
+
+    });
+        $('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function(){
+		$(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
+		$(this).addClass('selected');
+		$(this).find('input[type="radio"]').prop("checked", true);
+		
+	});
+
+});
+
+
